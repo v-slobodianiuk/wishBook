b0b0904020b4114c8a737397e39dad12167d9697
Vadym Slobidianiuk <dev.slobodianiuk@gmail.com>
2021-08-15 20:08:54 +0300

    Add Firebase storage service

---
+++ WishBook/Services/FirebaseStorageService.swift
@@ -0,0 +1,13 @@
+//
+//  FirebaseStorageService.swift
+//  WishBook
+//
+//  Created by Vadym Slobodianiuk on 15.08.2021.
+//
+
+import UIKit
+import FirebaseStorage
+
+struct FirebaseStorageService {
+
+}
