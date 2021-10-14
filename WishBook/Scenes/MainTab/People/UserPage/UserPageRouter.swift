8f8971f4aad7073ff9d3ee15e500aa94b781cc61
Vadym Slobidianiuk <dev.slobodianiuk@gmail.com>
2021-10-14 14:46:38 +0300

    UserPageRouter. Add showPeople func

--- WishBook/Scenes/MainTab/People/UserPage/UserPageRouter.swift
+++ WishBook/Scenes/MainTab/People/UserPage/UserPageRouter.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 protocol UserPageRouterProtocol: WishListRouterProtocol {
-
+    func showPeople<V: View>(filter: PeopleFilter, content: () -> V) -> AnyView
 }
 
 final class UserPageRouter: UserPageRouterProtocol {
     func showWishDetails(wishItem: WishListModel?) -> AnyView {
         let wishDetailsView = WishDetailsModuleBuilder.create(wishItem: wishItem)
         return AnyView(wishDetailsView)
+    }
+    
+    func showPeople<V: View>(filter: PeopleFilter, content: () -> V) -> AnyView {
+        let peopleView = PeopleModuleBuilder().create(filter: filter)
+        let navLink = NavigationLink(destination: peopleView) {
+            content()
+        }
+        return AnyView(navLink)
     }
 }
